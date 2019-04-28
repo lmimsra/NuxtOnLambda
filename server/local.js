8818eed859f21b2c51f6010924a89134e9b63613
Lima <lmimsra07@gmail.com>
2019-04-28 16:41:32 +0900

    ローカル実行用のファイル新規作成

---
+++ server/local.js
@@ -0,0 +1,44 @@
+// config.dev = !(process.env.NODE_ENV === 'production')
+const express = require('express')
+
+const consola = require('consola')
+const { Nuxt, Builder } = require('nuxt')
+const app = express()
+
+const host = process.env.HOST || 'localhost'
+const port = process.env.PORT || 3000
+
+// Import and Set Nuxt.js options
+const config = require('../nuxt.config.js')
+config.dev = !(process.env.NODE_ENV === 'production')
+
+async function start() {
+  // Init Nuxt.js
+  const nuxt = new Nuxt(config)
+
+  // const { host, port } = nuxt.options.server
+
+  // Build only in dev mode
+  if (config.dev) {
+    const builder = new Builder(nuxt)
+    await builder.build()
+    app.use(function(req, res, next) {
+      // eslint-disable-next-line no-console
+      console.log('[info]', 'Request URL: ', req.url)
+      next()
+    })
+  } else {
+    await nuxt.ready()
+  }
+
+  // Give nuxt middleware to express
+  app.use(nuxt.render)
+
+  // Listen the server
+  app.listen(port, host)
+  consola.ready({
+    message: `Server listening on http://${host}:${port}`,
+    badge: true
+  })
+}
+start()
