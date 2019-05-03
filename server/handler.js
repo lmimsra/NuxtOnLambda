be52f2ce8acca9d718fb25c89b51c703aaca2ac0
Lima <lmimsra07@gmail.com>
2019-05-03 21:59:26 +0900

    packageのコマンド追加、expressの対応メディアタイプを追加

--- server/handler.js
+++ server/handler.js
@@ -56,7 +56,28 @@ app.use(async (req, res, next) => {
   nuxt.render(req, res, next)
 })
 
-const server = awsServerlessExpress.createServer(app)
+const binaryMimeTypes = [
+  'application/javascript',
+  'application/json',
+  'application/octet-stream',
+  'application/xml',
+  'font/eot',
+  'font/opentype',
+  'font/otf',
+  'image/jpeg',
+  'image/png',
+  'image/svg+xml',
+  'image/x-icon',
+  'text/comma-separated-values',
+  'text/css',
+  'text/html',
+  'text/javascript',
+  'text/plain',
+  'text/text',
+  'text/xml'
+]
+
+const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes)
 
 module.exports.render = (event, context, callback) => {
   // eslint-disable-next-line no-console
