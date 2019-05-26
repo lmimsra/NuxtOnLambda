e5b40b9efe6109856b5faa6e52f40c927e0a00cf
Lima <lmimsra07@gmail.com>
2019-05-26 23:21:57 +0900

    リクエスト時のステータスで条件分岐ができるように更新

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 import express from 'express'
-
 const app = express()
 
-app.route('/').get(function(req, res) {
+app.route('/:status').all(function(req, res) {
   // eslint-disable-next-line no-console
   console.log('send')
-  res.send({ message: 'api success' })
+  res.send({
+    message: 'api success',
+    status: parseInt(req.params.status, 10)
+  })
 })
 
 export default {
